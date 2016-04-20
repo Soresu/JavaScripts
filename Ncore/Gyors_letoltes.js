a2a5ae03a8b9167cec1913b80c162500a218d1bd
Soresu <g_mester@freemail.hu>
2016-04-20 12:51:41 +0200

    GG Google

--- Ncore/Gyors_letoltes.js
+++ Ncore/Gyors_letoltes.js
@@ -1,30 +1,43 @@
 // ==UserScript==
-// @name        Ncore Gyors Letöltés
+// @name        Ncore Gyors LetÃ¶ltÃ©s
 // @namespace   https://ncore.cc/torrents.php
-// @description Ncore Gyors Letöltés
+// @description Ncore Gyors LetÃ¶ltÃ©s
 // @include     https://ncore.cc/torrents.php*
 // @version     1
 // @grant       none
 // ==/UserScript==
 //By Soresu
 
-var $ = unsafeWindow.jQuery;
-var torrents = []
-var link = '<div class="torrent_also"><a style="margin-left:3px;" href="https://ncore.cc/torrents.php?action=download&id=[ID]"><b>Letöltés</b></a> </div>';
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
 
-$(window).load(function () {
-	var index = 0;
-	$('a[href*="action=details&id="]').each(function (index) {
-		var id = /id=([0-9])\w+/.exec($(this).attr('href'))[0];
-		id = id.substr(3, id.length - 3);
-		if (torrents.indexOf(id) < 0) {
-			var torrent_txt = $(this).parent();
-			if (parseInt(torrent_txt.css('margin-top')) > 5) {
-				torrent_txt.css('margin-top', "1px");
-			}
-			torrent_txt.append(link.replace("[ID]", id)); ;
-			torrents[index] = id;
-			index++;
-		}
-	});
-});
+function main() {
+    var torrents = []
+    var link = '<div class="torrent_also"><a style="margin-left:3px;" href="https://ncore.cc/torrents.php?action=download&id=[ID]"><b>LetÃ¶ltÃ©s</b></a> </div>';
+
+    $(window).load(function () {
+        var index = 0;
+        $('a[href*="action=details&id="]').each(function (index) {
+            var id = /id=([0-9])\w+/.exec($(this).attr('href'))[0];
+            id = id.substr(3, id.length - 3);
+            if (torrents.indexOf(id) < 0) {
+                var torrent_txt = $(this).parent();
+                if (parseInt(torrent_txt.css('margin-top')) > 5) {
+                    torrent_txt.css('margin-top', parseInt(torrent_txt.css('margin-top'))/2+"px");
+                }
+                torrent_txt.append(link.replace("[ID]", id)); ;
+                torrents[index] = id;
+                index++;
+            }
+        });
+    });
+}
+
